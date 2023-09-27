fa70a5fdde48600e77a3526dae33661a6976fd8d
xaxoman <xhafaj.dennis@gmail.com>
2023-09-27 11:16:28 +0200

    Added User input control and random choice by computer

--- Projects/JS/rock-paper-scissors/main.js
+++ Projects/JS/rock-paper-scissors/main.js
@@ -1,24 +1,34 @@
-function getComputerChoice(){
+ let punteggioComputer = 0; 
+ let punteggioUser = 0;
+ 
+  
+  while(punteggioUser || punteggioComputer!=3) { // the for loop ends when the score is equal to 3 
 
-    let rock = 0;
-    let paper = 1;
-    let scissors = 2;
-    let random = Math.floor(Math.random() * 3);
-    if(random == rock) {
+        function getComputerChoice(){
 
-        console.log("Computer: ROCK");
+            let rock = 0;
+            let paper = 1;
+            let scissors = 2;
+
+            // User input
+            let user = prompt("Rock paper or scissors, choose wisely? (write lowercase)");
+            // generates a random number that is equal to the first three variables
+    
+            let random = Math.floor(Math.random() * 3);
+            // controlls the user input and calculates the score
+            if(user == "rock" && random == rock) {  
+                
+                alert(`Both choose rock, interesting \n COMPUTER: ${punteggioComputer} YOU: ${punteggioUser}`)
+            }
+            
+
+
+        }
+
+        getComputerChoice();
+
     }
 
-    else if(random == paper) {
+    
 
-        console.log("Compuer: PAPER");
-    }
-
-    else if(random == scissors) {
-
-        console.log("Compuer: SCISSORS");
-    }
-
-}
-
-getComputerChoice();
+    
